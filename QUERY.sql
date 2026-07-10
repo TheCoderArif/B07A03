914ce679e0ff8c3efe5b5deac3b1520b461fcfb9
TheCoderArif <mdarifulislam18539@gmail.com>
2026-07-10 10:08:45 +0600

    adding queries

--- QUERY.sql
+++ QUERY.sql
@@ -1,5 +1,8 @@
-create database World_Cup_Management;
+-- create database
+create database Football_Matches_Management;
 
+
+-- creating tables 
 create table users (
   user_id serial primary key,
   full_name varchar(50),
@@ -18,7 +21,6 @@ create table matches (
     check (match_status in ('Available', 'Selling Fast', 'Sold Out', 'Postponed'))
 );
 
-
 create table bookings (
   booking_id serial primary key,
   user_id smallint references users(user_id),
@@ -32,14 +34,35 @@ create table bookings (
 
 
 
+-- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
+
+select * from matches 
+where tournament_category = 'Champions League' and match_status = 'Available'
 
 
 
+-- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
+
+select * from users
+where full_name ilike 'Tanvir%'
+   or full_name ilike '%Haque%';
 
 
 
+-- Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
+
+select
+    booking_id,
+    user_id,
+    match_id,
+    seat_number,
+    coalesce(payment_status, 'Action Required') as payment_status,
+    total_cost
+from bookings;
 
 
+
+-- Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams.
 
 
 
